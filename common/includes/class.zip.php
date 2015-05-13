60b691b8824b8f4e6f2de0c2b1dc9dc95cd25335
Mark Roszko <mark.roszko@gmail.com>
2015-05-13 01:33:49 -0400

    Fix class.zip.php formatting

--- common/includes/class.zip.php
+++ common/includes/class.zip.php
@@ -13,78 +13,87 @@ require_once('common/pclzip/pclzip.lib.php');
  */
 class Zip
 {
-    private $archive;
-    private $filename;
-    private $errors;
-    private $fileList;
+	private $archive;
+	private $filename;
+	private $errors;
+	private $fileList;
 
-    /**
+	/**
 	 * Basic zip file parser. It allows you to extract zip files, create
-     * zips, but not expand existing zips.
-     */
-    function Zip($fileName)
-    {
-	$this->filename = $fileName;
-    }
-
-    /*
-	 * Adds but one file to a zip's internal list
-     */
-    function addFile($fileName)
-    {
-	$this->fileList[] = $fileName;
-    }
-
-    /**
-	 * Adds a list of files to the zip's index
-     */
-    function addFileArray($fileNames)
-    {
-	$this->fileList = $fileNames;
-    }
-
-    function createZip()
-    {	//check that the files we're adding exist, else remove them
-	if(count($this->fileList) == 0)
-	    return false;
-	foreach($this->fileList as $index => $oneFile)
+	 * zips, but not expand existing zips.
+	 */
+	function Zip($fileName)
 	{
-	    if(!file_exists($oneFile))
-		unset($this->fileList[$index]);
+		$this->filename = $fileName;
 	}
 
-	//actually create the zip file
-	$this->archive = new PclZip($this->filename);
-	$this->errors = $this->archive->create($this->fileList);
-	return true;
-    }
+	/*
+	 * Adds but one file to a zip's internal list
+	 */
+	function addFile($fileName)
+	{
+		$this->fileList[] = $fileName;
+	}
 
-    function extractZip($pathToExtractTo) {
-	if(!isset($this->archive)) $this->archive = new PclZip($this->filename);
-	$this->errors =$this->archive->extract(PCLZIP_OPT_PATH, $pathToExtractTo, PCLZIP_OPT_REPLACE_NEWER);
-    }
+	/**
+	 * Adds a list of files to the zip's index
+	 */
+	function addFileArray($fileNames)
+	{
+		$this->fileList = $fileNames;
+	}
 
-    function extractFile($index) {
-	if(!isset($this->archive)) $this->archive = new PclZip($this->filename);
-	return $this->archive->extractByIndex($index, PCLZIP_OPT_EXTRACT_AS_STRING);
-    }
+	function createZip()
+	{
+		//check that the files we're adding exist, else remove them
+		if(count($this->fileList) == 0)
+			return false;
 
-    /**
+		foreach($this->fileList as $index => $oneFile)
+		{
+			if(!file_exists($oneFile))
+			unset($this->fileList[$index]);
+		}
+
+		//actually create the zip file
+		$this->archive = new PclZip($this->filename);
+		$this->errors = $this->archive->create($this->fileList);
+		return true;
+	}
+
+	function extractZip($pathToExtractTo) {
+		if(!isset($this->archive))
+			$this->archive = new PclZip($this->filename);
+
+		$this->errors =$this->archive->extract(PCLZIP_OPT_PATH, $pathToExtractTo, PCLZIP_OPT_REPLACE_NEWER);
+	}
+
+	function extractFile($index) {
+		if(!isset($this->archive))
+			$this->archive = new PclZip($this->filename);
+
+		return $this->archive->extractByIndex($index, PCLZIP_OPT_EXTRACT_AS_STRING);
+	}
+
+	/**
 	 * Errors are returned as textual messages.
-     * This may get upgradedd to make use of proper codes.
-     */
-    function getErrors() {
-	if($this->errors == 0)
-	    return $this->archive->errorInfo(true);
-	else return false;
-    }
+	 * This may get upgradedd to make use of proper codes.
+	 */
+	function getErrors() {
+		if($this->errors == 0)
+			return $this->archive->errorInfo(true);
+		else
+			return false;
+	}
 
-    /**
+	/**
 	 * Gets the file list from an existing zip.
-     */
-    function getFileList() {
-	if(!isset($this->archive)) $this->archive = new PclZip($this->filename);
-	$this->errors = $this->archive->listContent();
-	return $this->errors;
-    }
+	 */
+	function getFileList() {
+		if(!isset($this->archive))
+			$this->archive = new PclZip($this->filename);
+
+		$this->errors = $this->archive->listContent();
+		return $this->errors;
+	}
 }
