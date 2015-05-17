8076cb50309a729873ff5fb7d0d4a91837e725cd
Mark Roszko <mark.roszko@gmail.com>
2015-05-17 13:46:30 -0400

    Remove set_include_path from index.php now that we tweaked the autoloader

--- index.php
+++ index.php
@@ -7,13 +7,10 @@ require __DIR__.'/vendor/autoload.php';
  */
 
 // Enable custom error handling.
-require_once ('common/includes/class.edkerror.php');
+require_once (__DIR__.'/common/includes/class.edkerror.php');
 
 set_error_handler(array('EDKError', 'handler'), E_ALL & ~(E_STRICT | E_NOTICE | E_USER_NOTICE) );
 @error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_USER_NOTICE));
 
-// Set up include paths.
-@set_include_path(get_include_path() . PATH_SEPARATOR . '.' . PATH_SEPARATOR . 'common' . PATH_SEPARATOR . 'includes');
-
 // Party time!
-include('common/index.php');
+include(__DIR__.'/common/index.php');
