2d9a617b79cccc15553f5c047f0c1a7f21781c9b
Mark Roszko <mark.roszko@gmail.com>
2015-05-23 22:36:08 -0400

    Replace EDK error handler with whoops.

--- bootstrap/edk.php
+++ bootstrap/edk.php
@@ -5,12 +5,14 @@ use EDK\Core\Config;
 /* Composer provides autoloading for us */
 require __DIR__.'/../vendor/autoload.php';
 
-
 include_once(__DIR__.'/../kbconfig.php');
 
-require_once (__DIR__.'/../common/includes/class.edkerror.php');
-set_error_handler(array('EDKError', 'handler'), E_ALL & ~(E_STRICT | E_NOTICE | E_USER_NOTICE) );
 @error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_USER_NOTICE));
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 
 
 require_once(__DIR__ . '/../common/includes/globals.php');
