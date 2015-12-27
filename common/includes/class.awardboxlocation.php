3e1cb69f07e3a8b3472f191acf7aaf037c8334ba
Mark Roszko <mark.roszko@gmail.com>
2015-12-27 14:39:30 -0500

    Fix AwardBoxLocation class to match __construct usage and formatting

--- common/includes/class.awardboxlocation.php
+++ common/includes/class.awardboxlocation.php
@@ -9,32 +9,29 @@
  */
 class AwardBoxLocation extends AwardBox
 {
-    
-        protected $limit_;
+	protected $limit_;
+
 	/**
 	 * Create an AwardBox from the given TopList and descriptions.
 	 */
-	function AwardBoxLocation($list, $title, $comment, $entity, $award, $limit = 10)
+	function __construct($list, $title, $comment, $entity, $award, $limit = 10)
 	{
-            parent::AwardBox($list, $title, $comment, $entity, $award, $limit);
+		parent::__construct($list, $title, $comment, $entity, $award, $limit);
 	}
 
-        
-        protected function getEntityImageUrl($row, $size)
-        {
-            $Location = new Location($row['itemID']);
-            return $Location->getIcon($size, false);
-        }
-        
-        protected function getEntityName($row)
-        {
-            return $row['itemName'];
-        }
-        
-        protected function getEntitytUrl($row)
-        {
-            return null;
-        }
-                
+	protected function getEntityImageUrl($row, $size)
+	{
+		$location = new Location($row['itemID']);
+		return $location->getIcon($size, false);
+	}
 
+	protected function getEntityName($row)
+	{
+		return $row['itemName'];
+	}
+
+	protected function getEntitytUrl($row)
+	{
+		return null;
+	}
 }
